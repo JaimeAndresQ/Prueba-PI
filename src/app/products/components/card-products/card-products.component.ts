4b5d55606bf03a2e75d28e89795a20e0171e603a
Jaime Quiñones <jaime.quinones.chacon@gmail.com>
2023-08-29 22:32:17 -0500

    Terminando vitrina

--- src/app/products/components/card-products/card-products.component.ts
+++ src/app/products/components/card-products/card-products.component.ts
@@ -12,6 +12,10 @@ export class CardProductsComponent {
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer
   ) {
+    this.matIconRegistry.addSvgIcon(
+      'carrito',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/shopping_cart.svg'),
+    )
   this.matIconRegistry.addSvgIcon(
     'cartas',
     this.domSanitizer.bypassSecurityTrustResourceUrl('../../../../assets/icons/cartaBorde.svg')
