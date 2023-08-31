6bac1ff2fd76e70facb7c31a6195fff3266713ef
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-08-31 16:26:46 -0500

    Update activate-page.component.ts

--- src/app/pages/registro/signup-page/activate-page.component.ts
+++ src/app/pages/registro/signup-page/activate-page.component.ts
@@ -1,3 +1,26 @@
 
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+export class ActivateComponent implements OnInit {
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });
+
+      this.http.post('http://login.thenexusbattles2.com:8001/api/activate/', registerData, { headers }).subscribe(
+        (response: any) => {
+          console.log('Registro exitoso:', response);
+        },
+        (error) => {
+          console.error('Error al registrar:', error);
+        }
+      );
+    }
+}
