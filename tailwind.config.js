8e1a7e6db2950096f1920f9e4e495ee7c7133487
John Doe <johndoe@example.com>
2023-09-14 00:51:26 -0500

    Nuevo Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sms': '340px',
+
+
+      },
+    },
+    negM: {
+      '1': '0.25rem', // Puedes ajustar este valor según tus necesidades
+      '2': '0.5rem', // Aquí puedes definir otros valores de espacio negativo
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
