9da418f273b579aefb531bd062ccc867819d2d6a
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-26 23:11:52 -0500

    listo ts

--- src/app/products/pages/products-page/categorias/items/items-page.component.ts
+++ src/app/products/pages/products-page/categorias/items/items-page.component.ts
@@ -213,7 +213,7 @@ export class ItemsPageComponent  implements OnInit, AfterViewChecked {
   }
 
   //boton para agregar al carrito de compras
-  addToCart(id_carta: string, price: number,nombre_carta: string) {
+  addToCart(Id: string, Precio: number,Nombre: string) {
     // Realizar una solicitud HTTP para agregar la carta al carrito
     const accessToken = localStorage.getItem('access_token');
 
@@ -227,7 +227,7 @@ export class ItemsPageComponent  implements OnInit, AfterViewChecked {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${accessToken}`
     });
-    const requestData = { id_carta: id_carta, price: price,nombre_carta: nombre_carta };
+    const requestData = { id_carta: Id, price: Precio,nombre_carta: Nombre };
 
     this.http.post(cartEndpoint, requestData, { headers }).subscribe(
         (response: any) => {
