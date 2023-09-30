967a9cafc83525fec957802c3ea81fe1eb0fdf6f
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-09-30 08:11:18 -0500

    Update product-detail.component.ts

--- src/app/products/pages/products-page/cardDetail/product-detail.component.ts
+++ src/app/products/pages/products-page/cardDetail/product-detail.component.ts
@@ -111,7 +111,7 @@ export class ProductDetailComponent  implements OnInit{
     }
 
     getComments(): void{
-      const apiUrl = `https://store.thenexusbattles2.cloud/comentarios/1`;
+      const apiUrl = `https://store.thenexusbattles2.cloud/comentarios/650f38ee7aaeb67f7dfc712e`;
       //const apiUrl = `http://alpha.bucaramanga.upb.edu.co:3000/api/comentariosCartas/1`;
       this.http.get<Comment[]>(apiUrl).subscribe(
           (data) => {
