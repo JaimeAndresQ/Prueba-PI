a264902f594cd6b117fbb47d12def4949704f199
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-10-17 01:59:06 -0500

    listo babnear usuarios

--- src/app/pages/administracion/administracion.component.ts
+++ src/app/pages/administracion/administracion.component.ts
@@ -38,8 +38,8 @@ export class AdministracionComponent implements OnInit{
         return;
     }
 
-    const cartEndpoint ='https://api.thenexusbattles2.cloud/login-api/api/getUsers/'
-    //const cartEndpoint = 'http://127.0.0.1:8002/api/getUsers';
+    //const cartEndpoint ='https://api.thenexusbattles2.cloud/login-api/api/getUsers/'
+    const cartEndpoint = 'http://127.0.0.1:8000/api/getUsers/?page=1';
     const headers = new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${accessToken}`
@@ -54,6 +54,34 @@ export class AdministracionComponent implements OnInit{
             console.error('Error al obtener los usuarios:', error);
         }
     );
+  }
+
+  changeActiveUser(user: string): void{
+    const accessToken = this.cookieService.get('access_token');
+
+    if (!accessToken) {
+        console.error('No se ha encontrado el token de acceso.');
+        return;
+    }
+
+    //const cartEndpoint ='https://api.thenexusbattles2.cloud/login-api/api/banUser/'
+    const cartEndpoint = 'http://127.0.0.1:8000/api/banUser/';
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+    });
+
+    const requestData = { user:user };
+  
+      this.http.post(cartEndpoint, requestData, { headers }).subscribe(
+          (response: any) => {
+              // Manejar la respuesta del servicio de carrito si es necesario
+              console.log('Ban response:', response);
+          },
+          (error) => {
+              console.error('Error baned user:', error);
+          }
+      );
   }
 
 }
