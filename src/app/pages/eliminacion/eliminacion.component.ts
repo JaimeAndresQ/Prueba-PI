2f443b56f0d357f3bb1c01b7be359bcab920efc1
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-10-17 23:14:19 -0500

    eliminar cuenta

--- src/app/pages/eliminacion/eliminacion.component.ts
+++ src/app/pages/eliminacion/eliminacion.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component,OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 
 
@@ -11,13 +13,38 @@ import { Component,OnInit } from '@angular/core';
 export class EliminacionComponent implements OnInit{
 
   constructor(
-
+    private http: HttpClient,private cookieService: CookieService
   ){}
 
   ngOnInit(): void {
 
   }
 
+  deleteUser(user: string): void{
+    const accessToken = this.cookieService.get('access_token');
 
+    if (!accessToken) {
+        console.error('No se ha encontrado el token de acceso.');
+        return;
+    }
+    const cartEndpoint ='https://api.thenexusbattles2.cloud/login-api/api/requests/'
+    //const cartEndpoint = 'http://127.0.0.1:8000/api/requests/';
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+    });
+
+    const requestData = { user:user };
+  
+      this.http.post(cartEndpoint, requestData, { headers }).subscribe(
+          (response: any) => {
+              // Manejar la respuesta del servicio de carrito si es necesario
+              console.log('delete response:', response);
+          },
+          (error) => {
+              console.error('Error delete user:', error);
+          }
+      );
+  }
 
 }
--- src/app/pages/perfil/perfil-page.component.ts
+++ src/app/pages/perfil/perfil-page.component.ts
@@ -100,23 +100,5 @@ export class ProfilePageComponent implements OnInit {
             return;
         }
         this.router.navigate(['/eliminacion'])
-        const cartEndpoint ='https://api.thenexusbattles2.cloud/login-api/api/requests/'
-        //const cartEndpoint = 'http://127.0.0.1:8000/api/requests/';
-        const headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${accessToken}`
-        });
-    
-        const requestData = { user:user };
-      
-          this.http.post(cartEndpoint, requestData, { headers }).subscribe(
-              (response: any) => {
-                  // Manejar la respuesta del servicio de carrito si es necesario
-                  console.log('delete response:', response);
-              },
-              (error) => {
-                  console.error('Error delete user:', error);
-              }
-          );
-      }
+    }
 }
